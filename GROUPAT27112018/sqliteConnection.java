5b62810d156f716edf893b58f10716321fc0dbab
JARAllen <44807899+JARAllen@users.noreply.github.com>
2018-11-29 17:09:03 -0500

    Update sqliteConnection.java

--- GROUPAT27112018/sqliteConnection.java
+++ GROUPAT27112018/sqliteConnection.java
@@ -8,17 +8,16 @@ public class sqliteConnection
 	public static Connection dbConnector()
 	{
 		try
-		{
+		{	//this enables connectivity requirement to SQlite DB. The location will need to be change
+			//the SQLlite DB is portable and requires no installation. it is accessible to the Java project at all ties within the folder
 			Class.forName("org.sqlite.JDBC");
-			Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\jara\\eclipse-workspace\\Login\\DB\\QuizDB.db");
-			//JOptionPane.showMessageDialog(null, "Successful Connection");
+			Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\jara\\eclipse-workspace\\Login\\Login\\DB\\QuizDB.db");
 			return conn;
 		}
 		catch(Exception e)
 		{
-			JOptionPane.showMessageDialog(null,"No Connection was made");
+			JOptionPane.showMessageDialog(null, e);
 			return null;
 		}
-		
-}
+	}
 }
